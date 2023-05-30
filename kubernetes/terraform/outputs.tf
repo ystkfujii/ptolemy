80a4ebfdcd0f2049b1e7c84a0979a8b2ec2a6637
ystkfujii <ystk.fujii0731@gmail.com>
2023-05-31 01:12:44 +0900

    feat: add worker_lb.dns_name to output

--- kubernetes/terraform/outputs.tf
+++ kubernetes/terraform/outputs.tf
@@ -5,3 +5,11 @@ output "k8s_infra_east" {
 output "k8s_infra_west" {
   value = module.k8s_infra_west
 }
+
+output "worker_lb_east" {
+  value       = nifcloud_load_balancer.east.dns_name
+}
+
+output "worker_lb_west" {
+  value       = nifcloud_load_balancer.west.dns_name
+}
